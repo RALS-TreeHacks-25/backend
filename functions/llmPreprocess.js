d49b7cc0383057866273e02ed7e59cb57bf9b99b
LarryPi314 <larryw1729@gmail.com>
2025-02-16 01:00:56 -0800

    smart user awareness

--- functions/llmPreprocess.js
+++ functions/llmPreprocess.js
@@ -22,6 +22,18 @@ async function getJournals(user) {
     }
 }
 
+async function prepareUserInfo(userId){
+    try {
+        const userDoc = await db.collection('users').doc(userId).get();
+        user = userDoc.data();
+        userInfo = `User Info: ${user.name}, ${user.age}, ${user.gender}, ${user.location}`;
+        return userInfo;
+    }
+    catch (error) {
+        console.log("error with getting user info");
+    }
+}
+
 // creates a string with all previous journal entries
 export async function preprocessJournalsLLM(userId) {
     const journals = await getJournals(userId);
